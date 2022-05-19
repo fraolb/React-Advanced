1e16af728d31f6090987c3c218a6e51c4182e849
Fraol Bereket <81419937+fraolb@users.noreply.github.com>
2022-05-19 09:09:23 +0300

    Add files via upload

---
+++ src/5-useRef/1-useRef-basics.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef } from 'react';
+
+// preserves value
+// DOES NOT trigger re-render
+// target DOM nodes/elements
+
+const UseRefBasics = () => {
+  const refContainer =useRef(null)
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(refContainer.current.value)
+  }
+ 
+  return <>
+    <form className='form' onSubmit={handleSubmit}>
+      <input type="text" ref={refContainer}/>
+      <button type="submit">submit</button>
+    </form>
+    
+  </>;
+};
+
+export default UseRefBasics;
